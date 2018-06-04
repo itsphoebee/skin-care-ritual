b2308704dfafeffa28386b7c331bfc844b6faa21
Phoebe Lau <phoebelau7@gmail.com>
2018-06-04 16:44:54 -0400

    edited item model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
   has_many :steps
   has_many :rituals, :through => :steps
-  validates :name, :brand, :price, :description, presence: true
+  validates :brand, :description, presence: true
+  validates :price, presence: true, numericality: true
+  validates :name, presence: true, uniqueness: {scope: :brand,
+    message: "Product items under a brand should have unique names"}
 end
