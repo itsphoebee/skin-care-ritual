1d734fe0920675ede4de9211b5ed55de0d49d390
Phoebe Lau <phoebelau7@gmail.com>
2018-07-15 12:17:39 -0400

    added carousel to landingpage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import photo from '../adult-hand-cream-hands-1029896.jpg'
-import { Button } from 'react-bootstrap';
+import { Carousel, Button } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <div>
-      <img src={photo} width="50%" height="50%" alt="home"/>
-      <h2>Your one stop for skin care treatments to rejuvenate your skin</h2>
+      <Carousel>
+      <Carousel.Item>
+        <img width={900} height={100} alt="900x700" src={photo} />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Your one stop for skin care treatments to rejuvenate your skin</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width={900} height={100} alt="900x500" src={photo} />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width={900} height={100} alt="900x500" src={photo} />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
       <a href="/rituals">
         <Button
           bsStyle="info"
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import App from './App';
